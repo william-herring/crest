53b9f85eda8af7b374cade1c80d67dccea64b764
William Herring <73984595+william-herring@users.noreply.github.com>
2022-12-01 07:46:28 +1100

    Create app CLI command implemented

--- crest/cli.py
+++ crest/cli.py
@@ -8,8 +8,14 @@ app = typer.Typer()
 @app.command()
 def create(title: str):
     working_directory = os.getcwd()
-    shutil.copytree('/resources/app_template', working_directory)
+    shutil.copytree(f'{os.path.dirname(os.path.realpath(__file__))}/resources/app_template', working_directory + '/app_template')
     os.rename('app_template', title)
+
+
+# Will be used to migrate schema changes once database module is fully implemented
+@app.command()
+def migrate(hi: int):
+    pass
 
 
 if __name__ == "__main__":
