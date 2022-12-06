2bb6b66634ddc2ee23e74ec6e23af34b3960c351
williamherring <william.herring.au@gmail.com>
2022-12-06 14:20:39 +1100

    Added templates.py

---
+++ crest/templates.py
@@ -0,0 +1,11 @@
+"""
+The Crest template engine is a collection of methods for interpreting and translating template syntax.
+"""
+
+
+def render(template: str, values: dict):
+    """
+    Crest templates are rendered by mapping the correct values to their declarations, and then inline logic
+    is run.
+    """
+    pass
