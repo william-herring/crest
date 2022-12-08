b88624f68e92b5fb64da7059d5ae2fe76d50972c
William Herring <73984595+william-herring@users.noreply.github.com>
2022-12-08 17:51:09 +1100

    Started template engine

--- crest/templates.py
+++ crest/templates.py
@@ -1,11 +1,27 @@
-"""
-The Crest template engine is a collection of methods for interpreting and translating template syntax.
-"""
-
-
-def render(template: str, values: dict):
+class Engine:
     """
-    Crest templates are rendered by mapping the correct values to their declarations, and then inline logic
-    is run.
+    The Crest template engine is a collection of methods for interpreting and translating template syntax.
     """
-    pass
+    def __init__(self):
+        self.statements = {
+            'if': self.handle_if,
+            'for': self.handle_for,
+        }
+
+    def handle_if(self, read_template, start_from):
+        pass
+
+    def handle_for(self, read_template, start_from):
+        pass
+
+    def render(self, template: str, props: dict):
+        """
+        Crest templates are rendered by mapping the correct values to their declarations, and then inline logic
+        is interpreted and evaluated.
+        """
+        with open(template, 'r') as temp:
+            content = temp.read()
+            for prop in props.keys():
+                content = content.replace('{ ' + prop + ' }', props.get(prop))
+
+            return content
